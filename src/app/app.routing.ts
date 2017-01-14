bf61f61b9fcc580804d981d56c04bd12b31abec8
Andre Teodoro <alteodoro@yahoo.com>
2017-01-14 19:28:02 -0200

    Adding the authentication mechanism

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule }   from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './users/register/register.component';
+import { LoginComponent } from './users/login/login.component';
+import { Angular2TokenService } from 'angular2-token';
 
 const appRoutes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent }
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 // Export the constant routing to be imported at the file app.module.ts
