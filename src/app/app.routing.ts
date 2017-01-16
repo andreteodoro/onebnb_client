3d8388d8ad90effb7f0104b9ec423d674ae7f0e8
Andre Teodoro <alteodoro@yahoo.com>
2017-01-16 16:29:16 -0200

    Implementing access control

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule }   from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './users/register/register.component';
 import { LoginComponent } from './users/login/login.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { Angular2TokenService } from 'angular2-token';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'user/edit', component: UserEditComponent, canActivate: [Angular2TokenService] }
 ];
 
 // Export the constant routing to be imported at the file app.module.ts
