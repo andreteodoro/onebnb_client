0106d242fce105829599a2b7c73aa08281c5f924
Andre Teodoro <alteodoro@yahoo.com>
2017-01-23 17:11:01 -0200

    Configuring environment variables

--- src/app/shared/properties.service.ts
+++ src/app/shared/properties.service.ts
@@ -5,40 +5,38 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Rx';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PropertiesService {
 
   private url: string = "http://localhost:3000/api/v1/properties.json";
   public resetPassword: boolean = false;
-  
+
   constructor(private http: Http) { }
 
-  getProperties() {
-    return this.http.get(this.url)
+  getProperties(){
+    return this.http.get(environment.api_base_url + 'properties.json')
       .map(res => res.json());
   }
 
-  getProperty(id) {
-    return this.http.get(this.url + '/' + id)
+  getProperty(id){
+    return this.http.get(environment.api_base_url + 'properties.json/' + id)
       .map(res => res.json());
   }
 
-  addProperty(property) {
-    return this.http.post(this.url, { 'property': property })
+  addProperty(property){
+    return this.http.post(environment.api_base_url + 'properties.json', {'property': property})
       .map(res => res.json());
   }
 
-  updateProperty(property) {
-    return this.http.put(this.url + '/' + property.id, { 'property': property })
+  updateProperty(property){
+    return this.http.put(environment.api_base_url + 'properties.json/' + property.id, {'property': property})
       .map(res => res.json());
   }
 
-  deleteProperty(id) {
-    return this.http.delete(this.url + '/' + id)
+  deleteProperty(id){
+    return this.http.delete(environment.api_base_url + 'properties.json/' + id)
       .map(res => res.json());
   }
-
-
-
 }
