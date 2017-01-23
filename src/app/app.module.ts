4bac828e5b342899d5fc6f7dabced85c0e937be6
Andre Teodoro <alteodoro@yahoo.com>
2017-01-23 20:50:10 -0200

    Adding components for search, pagination and filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { DropdownModule, AlertModule, ButtonsModule, DatepickerModule, CollapseModule, ModalModule} from "ng2-bootstrap/ng2-bootstrap";
+import { DropdownModule, AlertModule, ButtonsModule, DatepickerModule, CollapseModule, ModalModule, PaginationModule} from "ng2-bootstrap/ng2-bootstrap";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
@@ -17,6 +17,9 @@ import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { ResetPasswordComponent } from './users/reset-password/reset-password.component';
 import { UpdatePasswordComponent } from './users/update-password/update-password.component';
 import { ResultsComponent } from './results/results.component';
+import { SearchComponent } from './shared/search/search.component';
+import { FilterComponent } from './shared/filter/filter.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,10 @@ import { ResultsComponent } from './results/results.component';
     UserEditComponent,
     ResetPasswordComponent,
     UpdatePasswordComponent,
-    ResultsComponent
+    ResultsComponent,
+    SearchComponent,
+    FilterComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +44,9 @@ import { ResultsComponent } from './results/results.component';
     routing,
     AlertModule.forRoot(),
     DropdownModule.forRoot(),
-    CollapseModule
+    CollapseModule,
+    DatepickerModule.forRoot(),
+    PaginationModule.forRoot()
   ],
   providers: [PropertiesService, Angular2TokenService],
   bootstrap: [AppComponent]
