2924dd215233cc8830dd001984a90740f65bac76
Andre Teodoro <alteodoro@yahoo.com>
2017-01-31 21:50:13 -0200

    Fixing minor errors

--- src/app/shared/lightbox/lightbox.component.ts
+++ src/app/shared/lightbox/lightbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lightbox',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lightbox.component.scss']
 })
 export class LightboxComponent implements OnInit {
+
+  @Input() photos: any;
 
   constructor() { }
 
